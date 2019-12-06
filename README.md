a3829c66ecc86b98f0029d46ecc55bf495276339
Robert Poenaru <robert.poenaru@protonmail.ch>
2019-12-06 16:34:52 +0200

    adds backend and frontend structure. ready for implementation

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# elk_mean_loggingWebApp
+# ELK MEAN LOG UPLOADER WEB APP
 Upload files to MongoDB Atlas and to a local server. Send files to an ELK stack
---
+++ frontend/elk-log-uploader/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'elk-log-uploader'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('elk-log-uploader');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('elk-log-uploader app is running!');
+  });
+});
